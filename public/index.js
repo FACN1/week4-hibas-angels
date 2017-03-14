2e2545fbf8b154f0fc8578c3b6c638c78bdd3afb
mohamed omarii <mohamed.44301@hotmail.com>
2017-03-14 14:40:58 +0200

    request function

---
+++ public/index.js
@@ -0,0 +1,11 @@
+function makeRequest(url, callback){
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatsechange = function (){
+    if(xhr.readyState === 4 && xhr.status === 200){
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+  xhr.open('GET', url);
+  xhr.send();
+}
