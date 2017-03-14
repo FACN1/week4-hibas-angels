86693432719c3ad92aa21490c95522f060f6deac
Jack Davies <jackwldavies@icloud.com>
2017-03-14 17:45:30 +0200

    small syntax changes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
-
 function handler(request, response) {
   var url = request.url;
 
   if (url === "/") {
-    response.writeHead (200, {'Content-Type':'text/html'});
+    response.writeHead(200, {'Content-Type':'text/html'});
     fs.readFile(__dirname + '/../public/index.html', function(error,file) {
       if (error) {
         console.log(error);
@@ -16,7 +15,7 @@ function handler(request, response) {
     });
   }
   else if (url === "/index.js") {
-    response.writeHead (200, {'Content-Type':'text/js'});
+    response.writeHead(200, {'Content-Type':'text/js'});
     fs.readFile(__dirname + '/../public/index.js', function(error,file) {
       if (error) {
         console.log(error);
@@ -26,7 +25,7 @@ function handler(request, response) {
     });
   }
   else if (url === "/style.css") {
-    response.writeHead (200, {'Content-Type':'text/css'});
+    response.writeHead(200, {'Content-Type':'text/css'});
     fs.readFile(__dirname +  '/../public/style.css',function(error,file) {
       if (error) {
         console.log(error);
@@ -35,14 +34,14 @@ function handler(request, response) {
       response.end(file);
     });
   }
-  else{
-    response.writeHead (404, {'Content-Type':'text/plain'});
+  else {
+    response.writeHead(404, {'Content-Type':'text/plain'});
     response.end('the page not found');
   }
 }
 
 var server = http.createServer(handler);
 
-server.listen(3000, function(){
-  console.log("server is listing in 3000")
+server.listen(3000, function() {
+  console.log("server is listening in 3000");
 });
