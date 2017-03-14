a1ab5fc25cf3b1eb7d7dd914692399645949f22c
mohamed omarii <mohamed.44301@hotmail.com>
2017-03-14 15:27:25 +0200

    update indexJs null

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ function makeRequest(url, callback){
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
-      callback(err, JSON.parse(xhr.responseText));
+      callback(null, JSON.parse(xhr.responseText));
     }
   }
   xhr.open('GET',url);
