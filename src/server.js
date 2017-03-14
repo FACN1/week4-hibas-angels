07c31cd9fca9f62fe2adea2d32cadf93d342455c
Jack Davies <jackwldavies@icloud.com>
2017-03-14 17:22:18 +0200

    syntax tidying

--- src/server.js
+++ src/server.js
@@ -1,38 +1,38 @@
 var http = require('http');
 var fs = require('fs');
-var server = http.createServer(handler);
 
-server.listen(3000, function(){
-  console.log("server is listing in 3000")
+var server = http.createServer(handler);
+server.listen(3000, function() {
+  console.log("server is listening in 3000");
 });
 
-function handler (request,response){
-  var url= request.url;
+function handler(request,response) {
+  var url = request.url;
 
-  if(url ==="/"){
+  if (url === "/") {
     response.writeHead (200, {'Content-Type':'text/html'});
-    fs.readFile(__dirname + '/../public/index.html',function(error,file){
-      if(error){
+    fs.readFile(__dirname + '/../public/index.html', function(error,file) {
+      if (error) {
         console.log(error);
         return;
       }
       response.end(file);
     });
   }
-  else if (url ==="/index.js"){
+  else if (url === "/index.js") {
     response.writeHead (200, {'Content-Type':'text/js'});
-    fs.readFile(__dirname +  '/../public/index.js',function(error,file){
-      if(error){
+    fs.readFile(__dirname + '/../public/index.js', function(error,file) {
+      if (error) {
         console.log(error);
         return;
       };
       response.end(file);
     });
   }
-  else if (url ==="/style.css"){
+  else if (url === "/style.css") {
     response.writeHead (200, {'Content-Type':'text/css'});
-    fs.readFile(__dirname +  '/../public/style.css',function(error,file){
-      if(error){
+    fs.readFile(__dirname +  '/../public/style.css',function(error,file) {
+      if (error) {
         console.log(error);
         return;
       };
