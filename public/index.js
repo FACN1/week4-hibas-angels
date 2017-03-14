445dc74f599bdac8ceb13e73f47e49dff1d82cf9
Jack Davies <jackwldavies@icloud.com>
2017-03-14 15:05:42 +0200

    remove empty line

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
 function makeRequest(url, callback){
   var xhr = new XMLHttpRequest();
-
   xhr.onreadystatsechange = function (){
     if(xhr.readyState === 4 && xhr.status === 200){
       callback(JSON.parse(xhr.responseText));
