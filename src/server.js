01df26ea64e10aceb6542e6169d42b7c7ba52198
hiba <hibaakel@github.com>
2017-03-14 17:00:18 +0200

    make server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,42 @@
 var http = require('http');
-
-var server = http.createServer();
+var fs = require('fs');
+var server = http.createServer(handler);
 
 server.listen(3000, function(){
   console.log("server is listing in 3000")
 });
+
+function handler (request,response){
+  var url= request.url;
+
+  if(url ==="/"){
+    response.writeHead (200, {'Content-Type':'text/html'});
+    fs.readFile(__dirname + '/../public/index.html',function(error,file){
+      if(error){
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  }
+  else if (url ==="/index.js"){
+    response.writeHead (200, {'Content-Type':'text/js'});
+    fs.readFile(__dirname +  '/../public/index.js',function(error,file){
+      if(error){
+        console.log(error);
+        return;
+      };
+      response.end(file);
+    });
+  }
+  else if (url ==="/style.css"){
+    response.writeHead (200, {'Content-Type':'text/css'});
+    fs.readFile(__dirname +  '/../public/style.css',function(error,file){
+      if(error){
+        console.log(error);
+        return;
+      };
+      response.end(file);
+    });
+  }
+}
