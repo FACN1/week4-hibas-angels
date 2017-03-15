66ff162cebfbfb3e7b4cb7d34faaa41e0402874f
hiba <hibaakel@github.com>
2017-03-15 12:26:09 +0200

    we make response for request letters

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
+function getWords(letters){
+  return ["hi","hiba"];
+}
+
 function handler(request, response) {
   var url = request.url;
-
+  console.log(url);
   if (url === "/") {
-    response.writeHead(200, {'Content-Type':'text/html'});
+    response.writeHead (200, {'Content-Type':'text/html'});
     fs.readFile(__dirname + '/../public/index.html', function(error,file) {
       if (error) {
         console.log(error);
@@ -15,7 +19,7 @@ function handler(request, response) {
     });
   }
   else if (url === "/index.js") {
-    response.writeHead(200, {'Content-Type':'text/js'});
+    response.writeHead (200, {'Content-Type':'text/js'});
     fs.readFile(__dirname + '/../public/index.js', function(error,file) {
       if (error) {
         console.log(error);
@@ -25,7 +29,7 @@ function handler(request, response) {
     });
   }
   else if (url === "/style.css") {
-    response.writeHead(200, {'Content-Type':'text/css'});
+    response.writeHead (200, {'Content-Type':'text/css'});
     fs.readFile(__dirname +  '/../public/style.css',function(error,file) {
       if (error) {
         console.log(error);
@@ -34,14 +38,24 @@ function handler(request, response) {
       response.end(file);
     });
   }
-  else {
-    response.writeHead(404, {'Content-Type':'text/plain'});
+  else if(url.includes("/search")){
+    // we want the value after equals sign
+    var searchTerm =url.split('=')[1];
+    // the ruselt of the client search (its an array of words)
+    var wordArray = getWords(searchTerm);
+    // we send the response
+    response.writeHead (200, {'Content-Type':'text/plain'});
+    response.end(JSON.stringify(wordArray));
+  }
+  else{
+    response.writeHead (404, {'Content-Type':'text/plain'});
     response.end('the page not found');
   }
+
 }
 
 var server = http.createServer(handler);
 
-server.listen(3000, function() {
-  console.log("server is listening in 3000");
+server.listen(3000, function(){
+  console.log("server is listing in 3000")
 });
